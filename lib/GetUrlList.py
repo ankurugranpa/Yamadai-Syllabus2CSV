b3f9f20e564a51582e3d901d3f9920b32795a11e
ankuru <ankuru.ahahahaha@gmail.com>
2024-03-28 05:07:03 +0900

    add: [シラバスのhtmlからatagの抽出ライブラリの作成]

--- lib/GetUrlList.py
+++ lib/GetUrlList.py
@@ -7,7 +7,11 @@ class GetUrlList():
         self.base_url = f"https://www.yamagata-u.ac.jp/gakumu/syllabus/{self.year}"
 
 
-    def GetAtagList(self, target_url):
+    '''
+    return Atag URL List
+    ex)List[atag, atag2, atag3]
+    '''
+    def GetAtagList(self, target_url:str) -> list:
         try:
             result = []
             abs_target_url = self.base_url + target_url 
@@ -28,7 +32,34 @@ class GetUrlList():
             print("An error occurred:", str(e))
             return None
 
+    '''
+    return Atag URL List[tapple]
+    ex)List[(text, atag), (text2, atag2), (text3, atag3)]
+    '''
+
+    def GetAtagListText(self, target_url:str) -> list:
+        try:
+            result = []
+            abs_target_url = self.base_url + target_url 
+            response = requests.get(abs_target_url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                atags = soup.find_all('a')
+                if atags:
+                    for atag in atags:
+                        buf = atag.get_text(), atag.get("href"), 
+                        result.append(buf)
+                return result
+
+            else:
+                print("Failed to retrieve page:", response.status_code)
+                return None
+        except Exception as e:
+            print("An error occurred:", str(e))
+            return None
+
 
 if __name__ == "__main__":
     test = GetUrlList(2024)
     print(test.GetAtagList("/home.htm"))
+    print(test.GetAtagListText("/3cat.htm"))
