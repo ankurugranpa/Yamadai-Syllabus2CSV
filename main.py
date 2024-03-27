9938bde261e2db2ed6c482c35b3de53146f8a004
ankuru <ankuru.ahahahaha@gmail.com>
2024-03-28 05:08:16 +0900

    add: [メインプログラムの作成]

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-from lib.GetUrlList import GetUrlList
+import re
+import csv
+import sys
+import argparse
 
-GetUrlList = GetUrlList(2024)
+args = sys.argv
 
-DpUrlList = GetUrlList.GetAtagList("/home.htm")
-print(DpUrlList[-1])
+
+from lib.GetUrlList import GetUrlList as GetUrlList
+from lib.FacultyDict import FacultyDict
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--year",  type=int, default=2024, help="set  target year")
+    parser.add_argument("-d", "--dir",  type=str, default="./", help="set directory")
+    args = parser.parse_args()
+
+    year = args.year
+    directory = args.dir + "/"
+    GetUrl = GetUrlList(year)
+    faculty_dict = FacultyDict(year)
+
+
+    for course in faculty_dict.keys():
+        filename=f"{directory}{year}-{faculty_dict[course]}.csv"
+        print(f"Writing...{filename}")
+        kamoku_list = GetUrl.GetAtagListText(course)
+        with open(f"{filename}", 'w') as f:
+            writer = csv.writer(f)
+            for kamoku in kamoku_list:
+                jyugyou_list = GetUrl.GetAtagListText("/" + kamoku[1])
+                for jyugyou in jyugyou_list:
+                    writer.writerow(jyugyou)
+
+if __name__ == "__main__":
+    main()
