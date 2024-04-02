04f59448d1449bd03f4f3871252a2c5fc26a2085
ankuru <ankuru.ahahahaha@gmail.com>
2024-04-02 09:18:20 +0900

    add : [get_element関数のパフォーマンス改善とタイト数のカウンタ作成]
    get_element関数のGetDetailの値をcashを使用し複数回呼び出されても一度の計算で済むようにした
    またタイトルの数とサブタイトルの数がいくつあるのかを計算する関数の追加をした

--- syllabus/get_element.py
+++ syllabus/get_element.py
@@ -21,6 +21,8 @@ class GetElement():
 
     def __init__(self, url):
         self.url = url
+        self.__cash_detail_list:list = None
+        self.__cash_title_size:int = None
         response = requests.get(self.url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -45,47 +47,70 @@ class GetElement():
 
 
     def GetDetail(self) -> list:
-        detail_list = [] # [ (title, [(sub_title,naiyou), (title, [("",naiyou)]), (title, [(sub_title,naiyou), (sub_title, naiyou)]) ]
-        content_list = [] # [(sub_title,naiyou), (sub_title, naiyou)]
-        btag_list = self.element_list[2].find_all('b')
-        pattern = r'【.*?】'
-        is_sub_title = 0 # 【が含まれているかの判定, 1の時subタイトル
+        if self.__cash_detail_list is None:
+            detail_list = [] # [ (title, [(sub_title,naiyou), (title, [("",naiyou)]), (title, [(sub_title,naiyou), (sub_title, naiyou)]) ]
+            content_list = [] # [(sub_title,naiyou), (sub_title, naiyou)]
+            btag_list = self.element_list[2].find_all('b')
+            pattern = r'【.*?】'
+            is_sub_title = 0 # 【が含まれているかの判定, 1の時subタイトル
 
 
-        for i in range(len(btag_list)):
-            if (re.match(pattern, btag_list[i].get_text()) is not None):
-                 
-                if btag_list[i].find_next_sibling('div') != btag_list[i+1].find_next_sibling('div') \
-                        and i < len(btag_list)-1 :
-                    title_html = btag_list[i]
-                    title = title_html.get_text().lstrip("【").rstrip("】")
-                    content = title_html.find_next_sibling('div').get_text().lstrip("\n")
-                    tupple_buf = "", content
+            for i in range(len(btag_list)):
+                if (re.match(pattern, btag_list[i].get_text()) is not None):
+                     
+                    if btag_list[i].find_next_sibling('div') != btag_list[i+1].find_next_sibling('div') \
+                            and i < len(btag_list)-1 :
+                        title_html = btag_list[i]
+                        title = title_html.get_text().lstrip("【").rstrip("】")
+                        content = title_html.find_next_sibling('div').get_text().lstrip("\n")
+                        tupple_buf = "", content
+                        content_list.append(tupple_buf)
+                        tupple_buf = title, content_list
+                        detail_list.append(tupple_buf)
+                        content_list = []
+
+                else:
+                    sub_title_html = btag_list[i]
+                    sub_title = sub_title_html.get_text()
+                    content = sub_title_html.find_next_sibling('div').get_text().lstrip("\n")
+                    tupple_buf = sub_title, content
                     content_list.append(tupple_buf)
-                    tupple_buf = title, content_list
-                    detail_list.append(tupple_buf)
-                    content_list = []
+                    is_sub_title += 1
 
-            else:
-                sub_title_html = btag_list[i]
-                sub_title = sub_title_html.get_text()
-                content = sub_title_html.find_next_sibling('div').get_text().lstrip("\n")
-                tupple_buf = sub_title, content
-                content_list.append(tupple_buf)
-                is_sub_title += 1
+                if is_sub_title != 0:
+                    if (i == len(btag_list)-1)  or (re.match(pattern, btag_list[i+1].get_text()) is not None) :
+                        tupple_buf = btag_list[i-is_sub_title].get_text(), content_list
+                        detail_list.append(tupple_buf)
 
-            if is_sub_title != 0:
-                if (i == len(btag_list)-1)  or (re.match(pattern, btag_list[i+1].get_text()) is not None) :
-                    tupple_buf = btag_list[i-is_sub_title].get_text(), content_list
-                    detail_list.append(tupple_buf)
+                        content_list = []
+                        is_sub_title = 0
+                self.__cash_detail_list = detail_list
 
-                    content_list = []
-                    is_sub_title = 0
+        return self.__cash_detail_list
 
-        return detail_list
 
     def GetClassNum(self) -> int:
         return int(self.element_list[4].get_text().split("-")[-1])
+
+
+    def GetYear(self) -> int:
+        return int(self.element_list[4].get_text().split("-")[-3])
+
+    
+    def GetTitleSize(self) -> int:
+        if self.__cash_title_size is None:
+            self.__cash_title_size = len(self.GetDetail())
+
+        return self.__cash_title_size
+
+
+    def GetSubTitleSize(self) -> int:
+        size = 0
+        for i in range(self.GetTitleSize()):
+            if len(self.GetDetail()[i][1]) > 1:
+                size += len(self.GetDetail()[i][1]) 
+
+        return size
 
 
 if __name__ == "__main__":
@@ -102,12 +127,16 @@ if __name__ == "__main__":
     test3 = GetElement(url3)
     test4 = GetElement(url4)
 
-    for item in test3.GetTitle():
-        print(item)
+    # for item in test3.GetTitle():
+    #     print(item)
 
-    for item in test3.GetOverview():
-        print(item)
+    # for item in test3.GetOverview():
+    #     print(item)
 
-    for item in test3.GetDetail():
-        print(item)
-    print(test3.GetClassNum())
+    # for item in test3.GetDetail():
+    #     print(item)
+    print(test3.GetYear())
+    print(test3.GetTitleSize())
+    # print(len(test3.GetDetail()[1][1]))
+    print(test3.GetSubTitleSize())
+
